d9ce042a49f6be0fc6a2c6e626054d6f251b4a1d
shaik shahul <133729258+shaikshahul443@users.noreply.github.com>
2024-03-04 09:29:57 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,25 +1,80 @@
 package com.pack1;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Bank 
 {
 	  static double balance = 0;
-
-	  public static void main(String[] args) 
+	  static String name;
+	  
+	  public static void main(String[] args) throws InterruptedException
 	  {
-	    Scanner sc = new Scanner(System.in);
+		
+		//  System.out.println(LocalDateTime.now());
+	    @SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
 	    int option = 0;
+	    
+	    System.out.println("/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /");
+	    Thread.sleep(500);
+	    System.out.println("/ /                                                                           / /");
+	    Thread.sleep(500);
+	    System.out.println("/ /                    Welcome to the STATE BANK OF INDIA!                    / /");
+	    Thread.sleep(500);
+	    System.out.println("/ /                                                                           / /");
+	    Thread.sleep(500);
+	    System.out.println("/ / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /");
+	    System.out.println("                                                          Date :"+ LocalDate.now());
+	    System.out.println("                                                          Time :"+ LocalTime.now());
+	    Thread.sleep(500);
+	    System.out.print('L');
+	    Thread.sleep(500);
+	    System.out.print('O');
+	    Thread.sleep(500);
+	    System.out.print('A');
+	    Thread.sleep(500);
+	    System.out.print('D');
+	    Thread.sleep(500);
+	    System.out.print('I');
+	    Thread.sleep(500);
+	    System.out.print('N');
+	    Thread.sleep(500);
+	    System.out.print('G');
+	    Thread.sleep(500);
+	    System.out.print('.');
+	    Thread.sleep(500);
+	    System.out.print('.');
+	    Thread.sleep(500);
+	    System.out.print('.');
+	    System.out.println();
+	    System.out.println("Insert your ATM card");
+	    System.out.println("Enter your name ===>");
+	    name=sc.nextLine();
+	   
+	    
+	    while(true) 
+	    {
+	    System.out.println("******Please Enter your PIN number******");
+	    int pin = sc.nextInt();
+	    int[] al= {1234,2345,3456,4567};
+	    if((int)al[0]==pin || (int)al[1]==pin || (int)al[2]==pin || (int)al[3]==pin)
+	    {
+	    	System.out.println("welcome "+name);
+	    
+	    
+	    System.out.println(al);
 	    while (option != 4) 
 	    {
-	      System.out.println("Welcome to the Bank of Java");
+	      
 	      System.out.println("1. Check Balance");
 	      System.out.println("2. Deposit");
 	      System.out.println("3. Withdraw");
 	      System.out.println("4. Exit");
+	      
 	      System.out.println("Enter an option: ");
 	      option = sc.nextInt();
-
 	      switch (option) 
 	      {
 	        case 1:
@@ -40,16 +95,24 @@ public class Bank
 	      }
 	    }
 	  }
-
+	    
+	    else
+	    {
+	    	
+	    	System.out.println("please Enter a valid Pin number!!!!");
+	    	
+	    }
+	    }
+	  }
 	  public static void checkBalance() 
 	  {
-	    System.out.println("Your current balance is $" + balance+"\n");
+	    System.out.println(name+" Your current balance is $" + balance+"\n");
 	  }
 
 	  public static void deposit() 
 	  {
 	    Scanner sc = new Scanner(System.in);
-	    System.out.print("Enter the amount to deposit: $");
+	    System.out.print(name+" Enter the amount to deposit: $");
 	    double amount = sc.nextDouble();
 	    balance += amount;
 	    System.out.println("$" + amount + " has been deposited to your account.");
@@ -70,11 +133,21 @@ public class Bank
 	      System.out.println("$" + amount + " has been withdrawn from your account.");
 	    }
 	    checkBalance();
+	   
 	  }
+	  
+	  
+	  
 
 	  public static void exit() 
 	  {
-	    System.out.println("Thank you for banking with us. Have a great day!");
+	    System.out.println("Thank you for banking with us "+name+". Have a great day!");
 	  }
-	}
+	
+	  
+	  
+}
+	   
+	
+	
 
